195256eebd9beccddc4ea57385b4ccb211ba2f61
Dillon Titcomb <dillontitcomb@gmail.com>
2018-01-29 14:30:30 -0800

    adds html

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,9 @@ function Dog(name, breed, age, color, adopted) {
   this.adopted = adopted;
 };
 
+Dog.prototype.display = function() {
+  return "Thanks! Your " + this.color + " dog, " + this.name + ", age " + this.age + " has been submitted.";
+}
 
 //User Interface Logic
 $(document).ready(function() {
@@ -20,14 +23,14 @@ $(document).ready(function() {
     var dogBreed = $("input#new-breed-name").val();
     var dogColor = $("input#new-color-name").val();
     var adoptedDog = $("#new-adopted").val();
-    var newDog = new Dog (dogName, dogAge, dogBreed, dogColor, adoptedDog);
+    var newDog = new Dog (dogName, dogBreed, dogAge, dogColor, adoptedDog);
 
 
-    $("#show-pet h2").text(newDog.name);
-    $(".age-name").text(newDog.age);
-    $(".breed-name").text(newDog.breed);
-    $(".color-name").text(newDog.color);
-    $(".adopted").text(newDog.adopted);
+    $("#show-pet h2").text(newDog.display());
+    // $(".age-name").text(newDog.age);
+    // $(".breed-name").text(newDog.breed);
+    // $(".color-name").text(newDog.color);
+    // $(".adopted").text(newDog.adopted);
   //
   // $(".pet").last().click(function() {
   //   $("#show-pet").show();
