5f0c3c523feca03a01ca1f96924f258185f2dd6f
Dillon Titcomb <dillontitcomb@gmail.com>
2018-01-29 10:32:44 -0800

    adds new fields

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,36 @@
 //Business logic
 
-function Dog(name, breed, age, color) {
+function Dog(name, breed, age, color, adopted) {
   this.name = name;
   this.breed = breed;
   this.age = age;
   this.color = color;
+  this.adopted = adopted;
+};
 
 
 //User Interface Logic
 $(document).ready(function() {
   $("form#new-dog").submit(function(event) {
     event.preventDefault();
+
+
     var dogName = $("input#new-dog-name").val();
     var dogAge = $("input#new-age-name").val();
     var dogBreed = $("input#new-breed-name").val();
     var dogColor = $("input#new-color-name").val();
-    var newDog = new Dog (dogName, dogAge, dogBreed, dogColor);
+    var adoptedDog = $("#new-adopted").val();
+    var newDog = new Dog (dogName, dogAge, dogBreed, dogColor, adoptedDog);
 
 
-  $(".pet").last().click(function() {
-    $("#show-pet").show();
     $("#show-pet h2").text(newDog.name);
     $(".age-name").text(newDog.age);
     $(".breed-name").text(newDog.breed);
     $(".color-name").text(newDog.color);
-
+    $(".adopted").text(newDog.adopted);
+  //
+  // $(".pet").last().click(function() {
+  //   $("#show-pet").show();
+  // });
   });
 });
